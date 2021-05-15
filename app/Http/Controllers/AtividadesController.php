eeedd6d11ce76330f536368b2fe02ebde9ecf68c
guilherme <guilhermemancini@gmail.com>
2021-05-15 16:23:30 -0300

    Corrigido problema de adicionar atividades
    On branch guilherme
    Changes to be committed:
    	modified:   app/Http/Controllers/AtividadesController.php
    	modified:   resources/views/atividades/form.blade.php
    	modified:   routes/web.php

--- app/Http/Controllers/AtividadesController.php
+++ app/Http/Controllers/AtividadesController.php
@@ -26,31 +26,43 @@ class AtividadesController extends Controller
         ]);
     }
 
-    public function add()
+    public function add($area_id = null)
     {
-        return view('atividades.form');
+        return view('atividades.form', [
+            'area_id'   => $area_id,
+            'areas'     => Area::select('id', 'sigla', 'nome')->get(),
+        ]);
     }
 
     public function update(Request $request, $id)
     {
         $request->validate([
-            'nome' => 'required|min:3|max:50|string',
+            'nome'      => 'required|min:3|max:50|string',
+            'area_id'   => 'required|numeric|exists:hd_areas,id',
         ]);
+
         $atividade = Atividade::where('id', $id)->first();
+
         if(!$atividade)
             return redirect()->route('atividades_index')->with('error', 'Esta atividade não existe');
+
         $atividade->update([
             'nome' => $request->input('nome'),
+            'area_id' => $request->input('area_id'),
         ]);
+
         return redirect()->route('atividades_index')->with('success', 'Atividade atualizada com sucesso');
     }
 
     public function delete(Request $request, $id)
     {
         $atividade = Atividade::where('id', $id)->first();
+
         if(!$atividade)
             return redirect()->route('atividades_index')->with('error', 'Esta atividade não existe');
+
         $atividade->delete();
+
         return redirect()->route('atividades_index')->with('success', 'Atividade deletada com sucesso');
     }
 
@@ -59,10 +71,14 @@ class AtividadesController extends Controller
     {
         $request->validate([
             'nome' => 'required|min:3|max:50|string',
+            'area_id' => 'required|numeric|exists:hd_areas,id',
         ]);
+
         Atividade::create([
             'nome' => $request->input('nome'),
+            'area_id' => $request->input('area_id'),
         ]);
+
         return redirect()->route('atividades_index')->with('success', 'Atividade criada com sucesso');
     }
 
--- resources/views/atividades/form.blade.php
+++ resources/views/atividades/form.blade.php
@@ -1,28 +1,40 @@
 @extends('layouts.page')
 
 @php
-    $edit = !! ($atividade ?? null);
-    $action = $edit ? route('atividades_update', $atividade->id) : route('atividades_store');
+$edit = !!($atividade ?? null);
+$action = $edit ? route('atividades_update', $atividade->id) : route('atividades_store');
 @endphp
 
 @section('content')
 
-    <form action="{{ $action }}" method="POST">
+    <form action="{{ $action }}" method="POST" class='row g-6'>
         @csrf
-        <fieldset>
-            <legend>
-                {{ $edit ? 'Editar' : 'Criar' }}  Atividade
-            </legend>
-            <div class="mb-3">
-                <label for="nome" class="form-label">Nome da atividade</label>
-                <input type="text" id="nome" name="nome" value="{{ old('nome') ?? $atividade->nome ?? null }}" class="form-control" placeholder='' _maxlength="50" >
+        <h6>
+            {{ $edit ? 'Editar' : 'Criar' }} Atividade
+        </h6>
+
+        <div class="col-md-6">
+            <label for="nome" class="form-label">Nome da atividade</label>
+            <input type="text" id="nome" name="nome" value="{{ old('nome') ?? ($atividade->nome ?? null) }}"
+                class="form-control" placeholder='' maxlength="50" required>
                 @error('nome')
 
                 @enderror
-            </div>
+        </div>
 
-            <button type="submit" class="btn btn-primary">{{ $edit ? 'Atualizar' : 'Cadastrar' }}</button>
-        </fieldset>
+        <div class="col-md-4">
+            <label for="area_id" class="form-label">Selecione Area</label>
+            <select id="area_id" name='area_id' class="form-select" required>
+                <option value='' {{ !$area_id ? 'selected' : '' }}>Escolha um problema</option>
+                @foreach ($areas as $area)
+                    <option {{ $area_id == $area->id ? 'selected' : '' }} value='{{ $area->id }}'>{{ $area->sigla . ' - ' . $area->nome }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="col-md-2 pt-3">
+            <button type="submit" class="btn btn-primary mt-3 w-100">{{ $edit ? 'Atualizar' : 'Cadastrar' }}</button>
+        </div>
     </form>
 
 @endsection

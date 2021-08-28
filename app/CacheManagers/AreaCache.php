0518f0b12e2c9f77eb762f8cc5b8bd7681e5e519
Tiago França <dev@tiagofranca.com>
2021-08-27 23:04:08 -0300

    criado o CacheManager AreaCache
    No ramo 53-st-47-transferir-chamado
    Mudanças a serem submetidas:
    	new file:   app/CacheManagers/AreaCache.php

---
+++ app/CacheManagers/AreaCache.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\CacheManagers;
+
+use App\Models\Area;
+use Illuminate\Support\Facades\Cache;
+use Str;
+use Arr;
+
+class AreaCache
+{
+    protected static $clear_cache       = false;
+    protected static $default_relations = [
+        'atividades',
+        'atendentes',
+    ];
+
+    public static function byId(int $area_id, array $relationships = null)
+    {
+        $relationships = $relationships === [] ? self::$default_relations : [];
+
+        $cache_key = Str::slug(Arr::query(['area_id' => $area_id, 'relationships' => $relationships]));
+
+        if(self::$clear_cache)
+            Cache::forget($cache_key);
+
+        return Cache::remember($cache_key, (30 * 60/*secs*/), function ()  use ($area_id, $relationships) {
+            return Area::with($relationships)->where('id', $area_id)->first();
+        });
+    }
+
+    public static function allWhithoutRelationships(array $attributes = [], int $limit = null)
+    {
+        $cache_key = Str::slug(Arr::query(['limit' => ($limit ? $limit : 'no-limit'), 'attributes' => $attributes]));
+
+        if(self::$clear_cache)
+            Cache::forget($cache_key);
+
+        return Cache::remember($cache_key, (30 * 60/*secs*/), function ()  use ($attributes, $limit) {
+            $query = Area::orderBy('id');
+
+            if($attributes)
+                $query->select($attributes);
+
+            if($limit)
+                $query->limit($limit);
+
+            return $query->get();
+        });
+    }
+
+    public function clearCache(bool $clear_cache = null)
+    {
+        self::$clear_cache = !! $clear_cache;
+    }
+
+}

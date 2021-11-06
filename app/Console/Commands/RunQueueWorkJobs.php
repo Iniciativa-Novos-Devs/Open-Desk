c5a00dfc6d9936b7f6c4a8339e6eaaaa7794a80b
Tiago França <tiago@tiagofranca.com>
2021-11-05 23:13:52 -0300

    comando queue:work-custom-long
    No ramo queue-work-custom-long
    Mudanças a serem submetidas:
    	new file:   app/Console/Commands/RunQueueWorkJobs.php

---
+++ app/Console/Commands/RunQueueWorkJobs.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RunQueueWorkJobs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'queue:work-custom-long';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Executa o queue work com timeout mais longo';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return $this->call('queue:work', [
+            '--stop-when-empty' => null,
+            '--timeout' => 120,
+        ]);
+    }
+}

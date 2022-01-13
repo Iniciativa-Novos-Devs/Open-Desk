c5e6817ff1d9365f63da8bc30ca2a6e6fcd51832
Tiago França <tiago@tiagofranca.com>
2022-01-12 22:04:26 -0300

    Ajuste para quando não pode utilizar a função exec
    No ramo FEAT-show-git-hash
    Mudanças a serem submetidas:
    	modified:   app/Libs/Helpers/GitHashVersion.php
    	modified:   config/version-by-git.php

--- app/Libs/Helpers/GitHashVersion.php
+++ app/Libs/Helpers/GitHashVersion.php
@@ -10,17 +10,33 @@ class GitHashVersion
 
     public static function get()
     {
-        if(!config('version-by-git.show_versions'))
+        if (!config('version-by-git.show_versions'))
         {
             return null;
         }
 
+        return \Illuminate\Support\Facades\Cache::remember('version_by_commit', 30 /*secs*/, function () {
+            if(config('version-by-git.use_exec') && function_exists('exec'))
+            {
+                return self::viaExec();
+            }
+
+            return self::viaFile();
+        });
+    }
+
+    /**
+     * function viaExec
+     *
+     * @return string
+     */
+    public static function viaExec()
+    {
         $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
 
-        if(config('version-by-git.show_commit_date'))
+        if (config('version-by-git.show_commit_date'))
         {
             $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
-            $commitDate->setTimezone(new \DateTimeZone('UTC'));
             $formatedCommitDate = $commitDate->format('Y-m-d H:i:s');
         }
 
@@ -29,5 +45,67 @@ class GitHashVersion
         $env = config('app.env', 'production');
 
         return sprintf("v%s.%s.%s-{$env}.%s %s", self::MAJOR, self::MINOR, self::PATCH, $commitHash, $formatedCommitDate);
+    }
+
+    /**
+     * function viaFile
+     *
+     * @return string
+     */
+    public static function viaFile()
+    {
+        $branch = self::get_current_git_branch();
+
+        if (config('version-by-git.show_commit_date'))
+        {
+            $commitDate = new \DateTime(trim(self::get_current_git_datetime($branch)));
+            $formatedCommitDate = $commitDate->format('Y-m-d H:i:s');
+        }
+
+        $formatedCommitDate = isset($formatedCommitDate) ? "({$formatedCommitDate})" : '';
+        $env = config('app.env', 'production');
+
+        $commitHash = substr(file_get_contents(base_path(".git/refs/heads/{$branch}")),0,7);
+
+        return sprintf("v%s.%s.%s-{$env}.%s %s", self::MAJOR, self::MINOR, self::PATCH, $commitHash, $formatedCommitDate);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public static function get_current_git_branch()
+    {
+        $file_name = sprintf(base_path('.git/HEAD'));
+
+        if (!file_exists($file_name))
+        {
+            return null;
+        }
+
+        $data  = file_get_contents($file_name);
+        $array = explode("/", $data);
+        $array = array_reverse($array);
+        return  trim("" . $array[0] ?? '');
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public static function get_current_git_datetime($branch = 'master')
+    {
+        $file_name = sprintf(base_path('.git/refs/heads/%s'), $branch);
+        $time = filemtime($file_name);
+        if ($time != 0)
+        {
+            return date("Y-m-d H:i:s", $time);
+        }
+        else
+        {
+            return  "time=0";
+        }
     }
 }
--- config/version-by-git.php
+++ config/version-by-git.php
@@ -1,9 +1,23 @@
 <?php
 
 return [
+    /**
+     * Se deve mostrar detalhes da versão baseada no commit
+     */
     'show_versions' => env('SHOW_VERSIONS', true),
 
+    /**
+     * Se deve mostrar a hora do commit
+     */
     'show_commit_date' => env('SHOW_COMMIT_DATE', (env('APP_ENV') !== 'production')),
 
+    /**
+     * Se deve mostrar a versão do commit no rodapé do site
+     */
     'show_versions_in_footer' => env('SHOW_VERSIONS_IN_FOOTER', true),
+
+    /**
+     * Se deve utilizar a função exec() para obter a versão do git
+     */
+    'use_exec' => env('USE_EXEC', true),
 ];
